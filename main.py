7bd5681861c1ba908a5d46a35b41d6718c148ae5
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-09 19:27:49 +0200

    Calculate Multiple Times

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-x = int(input("First Number: "))
-y = int(input("Second Number: "))
+while True:
+    x = int(input("First Number: "))
+    y = int(input("Second Number: "))
 
-op = input("Choose Operation ([+] [-] [*] [/]): ")
+    op = input("Choose Operation ([+] [-] [*] [/]): ")
 
-if op == '+':
-    ans = x + y
-elif op == '-':
-    ans = x - y
-elif op == '*':
-    ans = x * y
-elif op == '/':
-    ans = x / y
+    if op == '+':
+        ans = x + y
+    elif op == '-':
+        ans = x - y
+    elif op == '*':
+        ans = x * y
+    elif op == '/':
+        ans = x / y
 
-print(f"Your ans: {ans}")
+    print(f"Your ans: {ans}")
+    
+    again = input("Calculate Again(y/n): ")
+    if again.lower() == 'n':
+        break
